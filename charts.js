fa7ed015b1554b3df925d0802e8b249f402d9b59
Aadhesharjun Senthilkumar <109750707+AadhesharjunS@users.noreply.github.com>
2024-07-28 08:49:33 -0400

    Change margins

--- charts.js
+++ charts.js
@@ -1,7 +1,7 @@
 async function electricity() {
-  const margin = {top: 50, right: 20, bottom: 50, left: 20},
-        width = 1000 - margin.left - margin.right,
-        height = 800 - margin.top - margin.bottom;
+  const margin = {top: 20, right: 30, bottom: 40, left: 30},
+        width = 800 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
   const data = await d3.csv("https://aadhesharjuns.github.io/narrativecs416/data/combined.csv");
   
   const svg = d3.select("#electricgraph").append("svg")
