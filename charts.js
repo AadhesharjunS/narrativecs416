fe544ac9c85b7e911d2ea2755d60935ffc9c9e2c
Aadhesharjun Senthilkumar <109750707+AadhesharjunS@users.noreply.github.com>
2024-07-28 08:53:53 -0400

    Update margins

--- charts.js
+++ charts.js
@@ -1,5 +1,5 @@
 async function electricity() {
-  const margin = {top: 20, right: 30, bottom: 40, left: 70},
+  const margin = {top: 20, right: 30, bottom: 40, left: 80},
         width = 800 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
   const data = await d3.csv("https://aadhesharjuns.github.io/narrativecs416/data/combined.csv");
